90b6275d90ac0b909ea54a988ec66e36ca16e3ae
nazmariam <fedirko.mary@gmail.com>
2019-02-27 23:00:51 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import 'normalize.css'
-// import './sass/_pipboy.css'
 import './sass/main.scss'
+
+window.onload = () => {
+    document.querySelector('.play').addEventListener('click', function () {
+        let audio = document.getElementById("radio");
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+
+        this.classList.toggle('active');
+    })
+};
+
 
 
