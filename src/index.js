df709e0f8194f35f6eeda8bfbd66d4a94a815fe7
nazmariam <fedirko.mary@gmail.com>
2019-02-28 13:44:01 +0200

    responsive done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'normalize.css'
 import './sass/main.scss'
-
+import './sass/_media.scss'
 window.onload = () => {
     document.querySelector('.play').addEventListener('click', function () {
         let audio = document.getElementById("radio");
